beecdcb2c556a1ec46fd5d699ad705b1bbc88b1e
Luis <81855420+luisoos@users.noreply.github.com>
2022-12-01 17:42:10 +0000

    feat:✨create-react-app bootstrap, typescript support, tailwindcss, prettier

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p className="text-2xl font-bold flex align-center justify-center">Hello World!</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
